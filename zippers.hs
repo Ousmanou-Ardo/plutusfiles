8f71453ac5d1ee7cafdc63d07d8cfda0588637de
Ousmanou-Ardo <ousmanou@iut-dhaka.edu>
2022-02-23 23:32:17 -0500

    updated

--- zippers.hs
+++ zippers.hs
@@ -1,3 +1,18 @@
 type Name = String
 type Data = String
 data FSItem = File Name Data | Folder Name [FSItem] deriving (Show)
+
+
+myDisk :: FSItem
+myDisk =
+Folder "root"
+[ File "goat_yelling_like_man.wmv" "baaaaaa"
+, File "pope_time.avi" "god bless"
+, Folder "pics"
+[ File "ape_throwing_up.jpg" "bleargh"
+, File "watermelon_smash.gif" "smash!!"
+, File "skull_man(scary).bmp" "Yikes!"
+]
+, File "dijon_poupon.doc" "best mustard"
+, Folder "programs"
+[ File "fartwizard.exe" "10gotofart"
